a4abb151f4b885cc67074c13c6d6946a5404d79a
Bridgett Coyle <bacoyle0409@gmail.com>
2019-07-22 15:43:55 -0600

    Update readme with names of team members

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::UsersController < ApplicationController
+
+    def index
+      render json: UserSerializer.new(User.all)
+    end
+  
+    def show
+      render json: UserSerializer.new(User.find(params[:id]))
+    end
+  
+    def destroy
+      user = User.find(params["id"])
+      if user.destroy
+        render status: 204,
+        json: {
+          status: "success",
+        }
+      end
+    end
+  
+  end
---
+++ app/serializers/user_serializer.rb
@@ -0,0 +1,5 @@
+class UserSerializer
+    include FastJsonapi::ObjectSerializer
+    attributes :name, :email
+  end
+  
