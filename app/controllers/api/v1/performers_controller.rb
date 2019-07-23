dfd0f5c26bca68b3dcbf51e7ad41ea89625fbd92
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-23 11:25:09 -0600

    Updates performer creation to take in an instagram token

--- app/controllers/api/v1/performers_controller.rb
+++ app/controllers/api/v1/performers_controller.rb
@@ -8,17 +8,17 @@ class Api::V1::PerformersController < ApplicationController
     end
 
     def create
-      service = InstagramService.new(params["code"])
+      service = InstagramService.new(params[:performer][:instagram_token])
       user = service.get_user_data
-      performer = Performer.new(instagram_token: user[:access_token],
-                                bio: user[:user][:bio],
-                                photo: user[:user][:profile_picture],
-                                name: user[:user][:full_name],
-                                insta_url: "https://www.instagram.com/#{user[:user][:username]}/")
+      performer = Performer.new(instagram_token: params[:performer][:instagram_token],
+                                bio: user[:bio],
+                                photo: user[:profile_picture],
+                                name: user[:full_name],
+                                insta_url: "https://www.instagram.com/#{user[:username]}/")
       if performer.save
         render json: performer
       else
-        performer = Performer.find_by(instagram_token: user[:access_token])
+        performer = Performer.find_by(instagram_token: params[:performer][:instagram_token])
         render json: performer
       end
     end
--- app/services/instagram_service.rb
+++ app/services/instagram_service.rb
@@ -1,25 +1,23 @@
 class InstagramService
-  def initialize(code)
-    @code = code
+  def initialize(access_token)
+    @access_token = access_token
   end
 
   def get_user_data
-   get_json("/oauth/access_token")
+   get_json("v1/users/self/")
  end
 
  private
 
  def get_json(url)
-   response = conn
-   data = JSON.parse(response.body, symbolize_names: true)
+   response = conn.get(url)
+   data = JSON.parse(response.body, symbolize_names: true)[:data]
  end
 
  def conn
-   Faraday.new(url: "https://api.instagram.com/")
-   .post('oauth/access_token', {client_id: "b3445eaff7de4e8e8c29a7071e84540c",
-                                client_secret: "402247e52e1b4059acba7099fa34470a",
-                                grant_type: "authorization_code",
-                                redirect_uri: "http://dragme.us-east-2.elasticbeanstalk.com/auth/instagram/callback",
-                                code: @code})
- end
+    Faraday.new(url: "https://api.instagram.com/") do |f|
+      f.params['access_token'] = @access_token
+      f.adapter Faraday.default_adapter
+    end
+  end
 end
